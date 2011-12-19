989600470c071ba3421205f3dfb828216fc9e79c
Axel Leucht <Axel.Leucht@dokom.net>
2011-12-19 21:35:17 +0100

    RatingChange

---
+++ src/net/sourceforge/jibs/util/RatingChange.java
@@ -0,0 +1,24 @@
+package net.sourceforge.jibs.util;
+
+public class RatingChange {
+
+	double ratingA;
+	double ratingB;
+
+	public double getRatingA() {
+		return ratingA;
+	}
+
+	public void setRatingA(double ratingA) {
+		this.ratingA = ratingA;
+	}
+
+	public double getRatingB() {
+		return ratingB;
+	}
+
+	public void setRatingB(double ratingB) {
+		this.ratingB = ratingB;
+	}
+
+}
